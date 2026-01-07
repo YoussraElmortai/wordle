f904f1b56828abca9673669f032ae9eac56cf330
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2026-01-07 06:00:24 +0100

    added keyboard styling

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,9 +1,35 @@
+import "./Keyboard.css";
+
 const KEYBOARDS_ROWS = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
   ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
   ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Backspace"],
 ];
 
-const Keyboard = ({ onKeyPress }) => {};
+const Keyboard = ({ onKeyPress }) => {
+  return (
+    <div className='keyboard'>
+      {KEYBOARDS_ROWS.map((row, rowIndex) => {
+        return (
+          <div key={rowIndex} className='keyboard-row'>
+            {row.map((key) => {
+              return (
+                <button
+                  className={`keyboard-key ${
+                    key === "Backspace" || key === "Enter"
+                      ? "keyboard-key-wide"
+                      : ""
+                  }`}
+                >
+                  {key === "Backspace" ? "⌫" : key}
+                </button>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Keyboard;
