1437d468266e9a8ac1e7998d5f7ab8b67838e5e6
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2026-02-16 00:17:01 +0100

    Update randomword.js

--- src/randomword.js
+++ src/randomword.js
@@ -1,11 +1,27 @@
 export const getRandomWord = async () => {
   try {
     const response = await fetch(
-      'https://random-word-api.herokuapp.com/word?length=5'
+      "https://random-word-api.herokuapp.com/word?length=5",
     );
-    const [word] =  await response.json();
+    const [word] = await response.json();
     return word;
   } catch (e) {
     console.error("Error:", e);
+    // if undif fallbacks words
+    const fallbackWords = [
+      "BYTE",
+      "CYPH",
+      "LOGIC",
+      "ALIEN",
+      "ROBOT",
+      "NANOB",
+      "CLONE",
+      "SPACE",
+      "LASER",
+      "VIRUS",
+    ];
+    return fallbackWords[
+      Math.floor(Math.random() * fallbackWords.length)
+    ];
   }
 };
